3bb05c4bdfef7c62e2fe01be9b3847d5d1d6bb99
Malek BK <malekbk98@gmail.com>
2022-02-03 14:57:03 +0100

    Fix bugs

--- src/views/Conversations.vue
+++ src/views/Conversations.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div v-if="conversations.length == 0" class="screen-center">
+    <div v-if="!conversations" class="screen-center">
       <div class="spinner-border text-danger" role="status">
         <span class="sr-only"></span>
       </div>
@@ -17,7 +17,7 @@
         </div>
       </div>
 
-      <div class="row mt-2">
+      <div class="row mt-2" v-if="conversations.length > 0">
         <div class="col-sm-3" v-for="c in conversations" :key="c.id">
           <div class="card">
             <div class="card-body text-center bg-dark text-light">
@@ -34,6 +34,25 @@
           </div>
         </div>
       </div>
+
+      <!--No conversations found-->
+      <template v-else>
+        <div class="text-center text-danger mt-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="70"
+            height="70"
+            fill="currentColor"
+            class="bi bi-inbox-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M4.98 4a.5.5 0 0 0-.39.188L1.54 8H6a.5.5 0 0 1 .5.5 1.5 1.5 0 1 0 3 0A.5.5 0 0 1 10 8h4.46l-3.05-3.812A.5.5 0 0 0 11.02 4H4.98zm-1.17-.437A1.5 1.5 0 0 1 4.98 3h6.04a1.5 1.5 0 0 1 1.17.563l3.7 4.625a.5.5 0 0 1 .106.374l-.39 3.124A1.5 1.5 0 0 1 14.117 13H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .106-.374l3.7-4.625z"
+            />
+          </svg>
+          <p class="text-muted">No conversations found</p>
+        </div>
+      </template>
 
       <!-- Modal -->
       <b-modal id="addModal" title="Add a new conversation" hide-footer>
